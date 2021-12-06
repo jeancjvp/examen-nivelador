cffcb6d867365e30dffc60e89f7c784c0cc9762c
Jean <jeancjvp@gmail.com>
2021-12-06 01:18:31 -0500

    adding the cowsay method and clean the code funcionality

--- routes/cowsay.js
+++ routes/cowsay.js
@@ -1,40 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var cowsay = require('cowsay');
 
 /* GET (With param) CowSay. */
-router.get('/get/:cowsay', function(req, res, next) {
-    // Getting Parameter
-    let cowsayUser = req.params.cowsay;
-
-    // Validating Parameter
-    checkParameter(cowsayUser);
-
-    return res.send(cowsayUser);
-});
+router.get('/get/:cowsay', function(req, res, next) { return res.send(cowSay(req.params.cowsay)); });
 
 /* GET (With query) CowSay. */
-router.get('/get', function(req, res, next) {
-    // Getting Parameter
-    let cowsayUser = req.query.cowsay;
-
-    // Validating Parameter
-    cowsayUser = checkParameter(cowsayUser);
-
-    return res.send(cowsayUser);
-});
+router.get('/get', function(req, res, next) { return res.send(cowSay(req.query.cowsay)); });
 
 /* POST CowSay. */
-router.post('/post', function(req, res, next) {
-    // Getting Parameter
-    let cowsayUser = req.body.cowsay;
-
-    // Validating Parameter
-    cowsayUser = checkParameter(cowsayUser);
-
-    return res.send(cowsayUser);
-});
+router.post('/post', function(req, res, next) { return res.send(cowSay(req.body.cowsay)); });
 
 module.exports = router;
+
+/* Dynamic CowSay */
+function cowSay(cowSay) {
+    // Cow variables
+    var cowEyes = 'Oo';
+    var cowTongue = 'U';
+
+    // Validating Parameter
+    cowsayUser = checkParameter(cowSay);
+
+    // Print Cow Response in console
+    console.log(cowsay.say({text: cowsayUser, e: cowEyes, T: cowTongue}));
+
+    // Return Cow Response
+    return cowsay.say({text: cowsayUser, e: cowEyes, T: cowTongue});
+}
 
 /* Check Parameter */
 function checkParameter(cowSay) {
